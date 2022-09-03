41411f60ef271a926508801e3a4c585059803fa1
Ulises <ulises.torrella@gmail.com>
2022-09-03 13:47:44 -0300

    event scala3er

--- scala3/implicit_parameters.scala
+++ scala3/implicit_parameters.scala
@@ -1,33 +1,25 @@
-object Demostracion{
-  def funcion(using size: Int) = {
+object Demostracion:
+  def funcion(using size: Int) = 
     def getMap(using size: Int): Map[String, Int] = Map("Alice" -> size)
-    
     getMap("Alice")
-  };  
-}
 
 
-object Main{
- 
+object Main:
+
   given size: Int = 42 ;
 
-  def main = {
+  def main = 
     Demostracion.funcion
-  }
-}
 
-object Main2{
+object Main2:
  
   given size: Int = 22 ;
 
-  def main = {
+  def main = 
     Demostracion.funcion
-  }
   
-  def nested = {
+  def nested = 
     Main.main
-  }
-}
 
 Main.main
 Main2.main
